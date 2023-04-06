3419e40b3f987bd515ce2790cd9b19d75abc202c
ECNUwyzZL <707373265@qq.com>
2023-04-06 14:06:43 +0800

    finish the sidebar

--- src/component/SideBar/SideBar.tsx
+++ src/component/SideBar/SideBar.tsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { FaAngleDoubleLeft } from 'react-icons/fa'
 import './style.css'
 
 export const SideBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleClick = () => {
+    setIsExpanded(!isExpanded);
+  };
   return (
     <>
-      <div className="sidebar">
-       Terminal3
+      <div className={`sidebar ${isExpanded ? 'expanded' : 'collapsed'}`}>
+      {isExpanded ? 
+          <></>: 
+        <div onClick={handleClick} className="toggle-div">
+          Terminal3
+        </div>
+      }
+      {isExpanded ?
+      <div className="sidebar-content">
+        <div className="main-header">
+          <p>Terminal3</p>
+          <FaAngleDoubleLeft onClick={handleClick} style={{cursor: "pointer"}}/>
+        </div>
+      </div>: <></>}
     </div>
     </>
   );
